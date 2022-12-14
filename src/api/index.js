3c1e049c6b7772f0026d0cf3f01f757f76148d5d
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2022-12-14 08:36:16 +0530

    api update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const url = "https://mymemories0809.herokuapp.com/posts";
-const url = "https://memoriesserver.onrender.com";
+const url = "https://memoriesserver.onrender.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
